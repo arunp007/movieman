83cb91f73ad216b7650615755b0ee966bbea47ba
arunp007 <arunp4624@gmail.com>
2022-06-12 19:51:48 +0530

    Added Some Changes

--- moviemanproject/movieapp/urls.py
+++ moviemanproject/movieapp/urls.py
@@ -2,5 +2,20 @@ from django.urls import path
 from.import views
 
 urlpatterns = [
-    path('movie',views.movie)
+    path('movie',views.movie),
+    path('ninteenseventeen',views.ninteenseventeen, name='ninteenseventeen'),
+    path('gandhi', views.gandhi, name='gandhi'),
+    path('gladiator', views.gladiator, name='gladiator'),
+    path('hacksawridge', views.hacksawridge, name='hacksawridge'),
+    path('inception', views.inception, name='inception'),
+    path('interstellar', views.interstellar, name='interstellar'),
+    path('joker', views.joker, name='joker'),
+    path('logan', views.logan, name='logan'),
+    path('memento', views.memento, name='memento'),
+    path('revenant', views.revenant, name='revenant'),
+    path('sanandreas', views.sanandreas, name='sanandreas'),
+    path('seven', views.seven, name='seven'),
+    path('thedarkknight', views.thedarkknight, name='thedarkknight'),
+    path('thegodfather', views.thegodfather, name='thegodfather'),
+    path('thepursuitofhappyness', views.thepursuitofhappyness, name='thepursuitofhappyness')
 ]
--- moviemanproject/movieapp/views.py
+++ moviemanproject/movieapp/views.py
@@ -3,3 +3,49 @@ from django.http import HttpResponse
 
 def movie(request):
     return render(request, 'movieman.html')
+
+def ninteenseventeen(request):
+    return render(request, '1917.html')
+
+def gandhi(request):
+    return render(request, 'gandhi.html')
+
+def gladiator(request):
+    return render(request, 'gladiator.html')
+
+def hacksawridge(request):
+    return render(request,'hacksawridge.html')
+
+def inception(request):
+    return render(request, 'inception.html')
+
+def interstellar(request):
+    return render(request, 'interstellar.html')
+
+def joker(request):
+    return render(request, 'joker.html')
+
+def logan(request):
+    return render(request, 'logan.html')
+
+def memento(request):
+    return render(request, 'memento.html')
+
+def revenant(request):
+    return render(request, 'revenant.html')
+
+def sanandreas(request):
+    return render(request, 'sanandreas.html')
+
+def seven(request):
+    return render(request, 'seven.html')
+
+def thedarkknight(request):
+    return render(request, 'thedarkknight.html')
+
+def thegodfather(request):
+    return render(request, 'thegodfather.html')
+
+def thepursuitofhappyness(request):
+    return render(request, 'thepursuitofhappyness.html')
+
